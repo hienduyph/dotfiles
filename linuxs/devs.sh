574e764dc6a0e2753e1c11de10fff8653a05542d
hienduyph <hienduyph@gmail.com>
2020-09-01 23:02:10 +0700

    feat: new ubuntu install

---
+++ linuxs/devs.sh
@@ -0,0 +1,10 @@
+sudo snap install protobuf
+
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+
+sudo add-apt-repository ppa:longsleep/golang-backports
+
+sudo apt install -y \
+  nodejs\
+  golang-go\
+  gcc g++ make
