dd21624252835eb2f7759cfedc6383fb640e5c82
hienduyph <hienduyph@gmail.com>
2020-09-05 06:14:25 +0700

    feat: update fonts

--- linuxs/basic.sh
+++ linuxs/basic.sh
@@ -1,14 +1,12 @@
+#/bin/bash
+
+set -e
+
 sudo apt update && sudo apt install -y  curl wget python3-dev python3 python3-pip 
 echo 'Add Chrome Repo'
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - \
 && echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
 
-# Albert
-# echo 'Add Albert repo'
-# echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_20.04/ /' | sudo tee /etc/apt/sources.list.d/home:manuelschneid3r.list
-# curl -fsSL https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_20.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home:manuelschneid3r.gpg > /dev/null
-# 
-# Git
 echo 'Add Git Repo'
 sudo add-apt-repository ppa:git-core/ppa -y
 
@@ -49,15 +47,18 @@ sudo apt-get install -y \
   tmux\
   htop\
   flameshot\
-  zsh
+  zsh\
+  dnscrypt-proxy
 
 sudo usermod -aG docker $USER
-
-sudo systemctl start docker
-sudo systemctl enable docker
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 curl -sfL https://direnv.net/install.sh | sudo bash
 
 ibus restart
 
+
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo systemctl start dnscrypt-proxy
+sudo systemctl enable dnscrypt-proxy
