0a13a5f6b941b02357075a8bd0f8d06899c6a3cc
hienduyph <hienduyph@gmail.com>
2020-09-12 05:59:57 +0700

    feat: brew(neovim), dns manager

---
+++ linuxs/extensions/kubectl.sh
@@ -0,0 +1,11 @@
+_brew() {
+  set -x; cd "$(mktemp -d)" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/krew.tar.gz" &&
+  tar zxvf krew.tar.gz &&
+  KREW=./krew-"$(uname | tr '[:upper:]' '[:lower:]')_amd64" &&
+  "$KREW" install krew
+}
+
+tee ~/.profile -a << EOF
+export PATH="$HOME/.krew/bin:$PATH"
+EOF
