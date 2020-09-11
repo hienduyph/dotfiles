738ff36b3fc7150fe07cb377caa3db07536c8054
hienduyph <hienduyph@gmail.com>
2020-09-12 05:59:57 +0700

    feat: brew(neovim), dns manager

--- linuxs/extensions/network.sh
+++ linuxs/extensions/network.sh
@@ -6,8 +6,8 @@ echo "Setting Nameserver"
 
 sudo tee /etc/systemd/resolved.conf << END
 [Resolve]
-DNS=127.0.0.1
-#FallbackDNS=
+DNS=127.0.0.1 8.8.8.8 1.1.1.1
+FallbackDNS=9.9.9.9
 #Domains=
 #LLMNR=no
 #MulticastDNS=no
