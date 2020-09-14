b85684b63cdd1f02f8e94f03159787bec4855017
hienduyph <hienduyph@gmail.com>
2020-09-14 07:38:34 +0700

    feat: add office bash

--- linuxs/basic.sh
+++ linuxs/basic.sh
@@ -21,11 +21,11 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
  $(lsb_release -cs) \
- stable"
+ stable" -y
 
 echo 'Add Ibus Repo'
 # ibus
-sudo add-apt-repository ppa:bamboo-engine/ibus-bamboo
+sudo add-apt-repository ppa:bamboo-engine/ibus-bamboo -y
 
 # Install all
 echo 'Install all package'
