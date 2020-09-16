1e075981dfcea4b7e1751ab345f62b6b4e6d3818
hienduyph <hienduyph@gmail.com>
2020-09-16 11:24:58 +0700

    feat: ubuntu on my laptop

--- linuxs/extensions/nvidia.sh
+++ linuxs/extensions/nvidia.sh
@@ -1,3 +1,10 @@
 sudo add-apt-repository ppa:graphics-drivers/ppa
 
 sudo apt install nvidia-driver-450 
+
+# Add the package repositories
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+
+sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
---
+++ linuxs/utils.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -e
+
+sudo apt install -y \
+	jq
