04f7b14a2203b77c12a077ee00b1136abc940b8a
hienduyph <hienduyph@gmail.com>
2020-09-20 20:32:12 +0700

    feat: add kvm & dockerx11

---
+++ linuxs/kvm/core.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+_kvm_core() {
+  sudo apt-get install -y \
+    qemu-system\
+    qemu-utils\
+    virt-manager\
+    qemu-kvm\
+    libvirt-daemon-system\
+    libvirt-clients bridge-utils
+
+  sudo adduser `id -un` libvirt
+  sudo adduser `id -un` kvm
+}
+
+_vagrant() {
+  version=${1:-2.2.10}
+  curl -Lo /tmp/vagrant.deb https://releases.hashicorp.com/vagrant/${version}/vagrant_${version}_x86_64.deb
+  sudo apt install /tmp/vagrant.deb
+  rm -f /tmp/vagrant.deb
+  sudo apt install libvirt-dev  -y
+
+  CONFIGURE_ARGS="with-libvirt-include=/usr/include/libvirt --with-libvirt-lib=/usr/lib/libvirt" vagrant plugin install vagrant-libvirt
+}
+
+_kvm_core
+_vagrant "2.2.10"
