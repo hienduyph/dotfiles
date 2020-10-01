56406a4caa41f4dfc45138b16efdca12c4cced89
hienduyph <hienduyph@gmail.com>
2020-10-01 17:22:44 +0700

    feat: git rls, albert key add command

--- linuxs/albert.sh
+++ linuxs/albert.sh
@@ -1,4 +1,5 @@
 echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_20.04/ /' | sudo tee /etc/apt/sources.list.d/home:manuelschneid3r.list
-curl -fsSL https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_20.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home:manuelschneid3r.gpg > /dev/null
+curl -fsSL https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_20.04/Release.key | sudo apt-key add - 
+
 sudo apt update
 sudo apt install albert
