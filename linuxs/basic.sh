b660a4213fd7d0503f7306061679a3d709714fb1
hienduyph <hienduyph@gmail.com>
2020-11-10 11:45:12 +0700

    feat: add python3-venv linux package

--- linuxs/basic.sh
+++ linuxs/basic.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-sudo apt update && sudo apt install -y  curl wget python3-dev python3 python3-pip 
+sudo apt update && sudo apt install -y  curl wget python3-dev python3 python3-pip python3-venv
 echo 'Add Chrome Repo'
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - \
 && echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
