4f480102438ea1096aa56298e94a8d1aaffd667d
hienduyph <hienduyph@gmail.com>
2020-11-14 12:44:19 +0700

    feat: using golang from brew

--- linuxs/extensions/devs.sh
+++ linuxs/extensions/devs.sh
@@ -4,15 +4,15 @@ sudo snap install code --classic
 
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 
-sudo add-apt-repository ppa:longsleep/golang-backports
 sudo add-apt-repository ppa:serge-rider/dbeaver-ce
 
-sudo apt install -y \
-  nodejs\
-  golang-go\
-  gcc g++ make\
-  libmysqlclient-dev\
-  libssl-dev\
-  libpq-dev \
-  dbeaver-ce	
+packages=(
+  nodejs
+  gcc g++ make
+  libmysqlclient-dev
+  libssl-dev
+  libpq-dev
+  dbeaver-ce
+)
 
+sudo apt install -y "$packages[@]"
