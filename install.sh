1390c5c9c34380107dc2869c1254ec3f840c38eb
hienduyph <hienduyph@gmail.com>
2020-11-15 21:38:57 +0700

    feat: rewrite dotfiles install scripts, docker-cli install script

--- install.sh
+++ install.sh
@@ -6,57 +6,69 @@ f_backups() {
   mv $1 ~/.backups/ || true
 }
 
-echo "Install Neovim Host"
-python3 -m pip install virtualenv
-python3 -m virtualenv ~/.venv/neovim
-~/.venv/neovim/bin/pip install neovim pylint black flake8 mypy pydocstyle
+_neovim() {
+  echo "Install Neovim Host"
+  NEOVIM_HOST=$HOME/.venv/neovim
+  python3 -m virtualenv $NEOVIM_HOST
+  $NEOVIM_HOST/bin/pip install neovim pylint black flake8 mypy pydocstyle
 
-echo "Install Neovim"
-f_backups ~/.config/nvim/init.vim
-f_backups ~/.config/nvim/coc-settings.json
+  echo "Install neovim config"
+  f_backups ~/.config/nvim/init.vim
+  f_backups ~/.config/nvim/coc-settings.json
 
-mkdir -p ~/.config/nvim
-ln -s ~/dotfiles/vim/init.vim ~/.config/nvim/init.vim
-ln -s ~/dotfiles/vim/coc-settings.json ~/.config/nvim/
+  mkdir -p ~/.config/nvim
+  ln -s ~/dotfiles/vim/init.vim ~/.config/nvim/init.vim
+  ln -s ~/dotfiles/vim/coc-settings.json ~/.config/nvim/
 
-# for nvim
-curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  # for nvim
+  curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-nvim +PlugInstall +qall
+  nvim +PlugInstall +qall
+}
 
-echo "Install zsh"
+_shell() {
+  mkdir -p ~/.zsh
+  curl -fsSL git.io/antigen > ~/.zsh/antigen.zsh
+  curl -fsSL https://starship.rs/install.sh | bash
+  # zsh config
+  mkdir -p ~/.zsh/completions/
+  rustup completions zsh cargo > ~/.zsh/completions/_cargo
+}
 
-mkdir -p ~/.zsh
-curl -L git.io/antigen > ~/.zsh/antigen.zsh
-
-dots=(
-  .zshrc
-  .profile
-  .gitconfig
-  .pylintrc
-  .htoprc
-  .ideavimrc
-  .tmux.conf
-)
-for f in "${dots[@]}"; do
-  f_backups $HOME/$f
-  ln -s ~/dotfiles/$f $HOME/$f
-done
-
-# zsh config
-mkdir -p ~/.oh-my-zsh/completions/
-rustup completions zsh cargo > ~/.oh-my-zsh/completions/_cargo
-
-platform="$(uname -s | tr '[:upper:]' '[:lower:]')"
-platform_dots=(
-  .alacritty.yaml
-)
-
-for f in "${platform_dots[@]}"; do
-  fullpath="~/dotfiles/$platform/$f"
-  if [ -f $fullpath ]; then
+_dots() {
+  dots=(
+    .zshrc
+    .profile
+    .gitconfig
+    .pylintrc
+    .htoprc
+    .ideavimrc
+    .tmux.conf
+  )
+  for f in "${dots[@]}"; do
     f_backups $HOME/$f
-    ln -s $fullpath $HOME/$f
-  fi
-done
+    ln -s ~/dotfiles/$f $HOME/$f
+  done
+
+  platform_dots=(
+    .alacritty.yaml
+  )
+  platform="$(uname -s | tr '[:upper:]' '[:lower:]')"
+
+  for f in "${platform_dots[@]}"; do
+    fullpath="~/dotfiles/$platform/$f"
+    if [ -f $fullpath ]; then
+      f_backups $HOME/$f
+      ln -s $fullpath $HOME/$f
+    fi
+  done
+}
+
+main() {
+  _neovim
+  _shell
+  _dots
+}
+
+main
