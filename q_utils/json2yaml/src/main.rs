3d63fb6079eb98170077cc1755be1108f24d2566
hienduyph <hienduyph@gmail.com>
2020-12-21 21:39:19 +0700

    feat: using binary from rust

--- q_utils/json2yaml/src/main.rs
+++ q_utils/json2yaml/src/main.rs
@@ -1,13 +1,14 @@
 use serde_json::Value;
 use std::io::Read;
 
-fn json_2_yaml(json: &str) -> String {
-    let map: Value = serde_json::from_str(json).unwrap();
-    return serde_yaml::to_string(&map).unwrap();
+fn json_2_yaml(json: &str) -> Result<String, String> {
+    let raw = serde_json::from_str::<Value>(json).map_err(|v| v.to_string())?;
+    serde_yaml::to_string(&raw).map_err(|e| e.to_string())
 }
 
-fn main() {
+fn main() -> Result<(), String> {
     let mut buffer = String::new();
     std::io::stdin().read_to_string(&mut buffer).unwrap();
-    println!("{}", json_2_yaml(&buffer));
+    println!("{}", json_2_yaml(&buffer)?);
+    Ok(())
 }
