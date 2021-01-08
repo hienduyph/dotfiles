acc575ecfe213de69be98055095d59c3fac1ac8c
HienPham <hien.pham2@tiki.vn>
2021-01-08 14:25:42 +0700

    feat: install for new mac

--- platform/darwin/docker-cli.sh
+++ platform/darwin/docker-cli.sh
@@ -3,17 +3,19 @@ set -e
 
 _docker_cli() {
   DOCKER_VESION=$1
+  echo "Download Docker compose $1"
   TMP_DIR=/tmp/dockerstages
   DEST_BIN=/usr/local/bin/docker
   sudo rm -rf $DEST_BIN
   mkdir -p $TMP_DIR
-  curl -fsSL https://download.docker.com/mac/static/stable/x86_64/docker-$DOCKER_VESION.tgz | tar xz -C /tmp/dockerstages
+  curl -fsSL https://download.docker.com/mac/static/stable/x86_64/docker-$DOCKER_VESION.tgz | tar xz -C /tmp/dockerstages --strip-components=1
   sudo mv $TMP_DIR/docker $DEST_BIN
   rm -rf $TMP_DIR
 }
 
 
 _docker_compose() {
+  echo "Download Docker-compose $1"
   COMPOSE_VERSION=$1
   INSTALL_DIR=/usr/local/docker-compose
   sudo rm -rf $INSTALL_DIR/ && sudo mkdir -p $INSTALL_DIR
@@ -22,5 +24,5 @@ _docker_compose() {
   sudo ln -s $INSTALL_DIR/docker-compose /usr/local/bin
 }
 
-_docker_cli "19.03.13"
+_docker_cli "20.10.2"
 _docker_compose "1.27.4"
