c740e6cb995f9b9c6c028496e2a46d5b85dc3177
hienduyph <hienduyph@gmail.com>
2021-03-03 10:03:40 +0700

    feat: org import & save formatter

--- vim/neovim/lsp.vim
+++ vim/neovim/lsp.vim
@@ -5,3 +5,8 @@ let g:completion_matching_strategy_list = ['exact', 'substring', 'fuzzy']
 inoremap <expr> <Tab>   pumvisible() ? "\<C-n>" : "\<Tab>"
 inoremap <expr> <S-Tab> pumvisible() ? "\<C-p>" : "\<S-Tab>"
 " -------------------- LSP ---------------------------------
+"
+" Add `:Format` command to format current buffer.
+command! -nargs=0 Format :lua vim.lsp.buf.formatting_sync()
+command! -nargs=0 OR :lua lsp_organize_imports()
+
--- vim/neovim/lua/lsp.lua
+++ vim/neovim/lua/lsp.lua
@@ -59,3 +59,27 @@ require'nvim-treesitter.configs'.setup {
     enable = true
   },
 }
+
+function lsp_organize_imports()
+  local context = { source = { organizeImports = true } }
+  vim.validate { context = { context, "table", true } }
+
+  local params = vim.lsp.util.make_range_params()
+  params.context = context
+
+  local method = "textDocument/codeAction"
+  local timeout = 1000 -- ms
+
+  local resp = vim.lsp.buf_request_sync(0, method, params, timeout)
+  if not resp then return end
+
+  for _, client in ipairs(vim.lsp.get_active_clients()) do
+    if resp[client.id] then
+      local result = resp[client.id].result
+      if not result or not result[1] then return end
+
+      local edit = result[1].edit
+      vim.lsp.util.apply_workspace_edit(edit)
+    end
+  end
+end
