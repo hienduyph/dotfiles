fc79a9ca90b188868f99d90b21e8dd0c20418474
hienduyph <hienduyph@gmail.com>
2021-03-22 20:19:01 +0700

    vim: separated list diagnostic client, change git go

--- vim/neovim/lua/lsp.lua
+++ vim/neovim/lua/lsp.lua
@@ -24,7 +24,6 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', '<leader>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
   buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
   buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-  buf_set_keymap('n', '<leader>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
 
   -- Set some keybinds conditional on server capabilities
   if client.resolved_capabilities.document_formatting then
@@ -59,7 +58,7 @@ nvim_lsp.gopls.setup {
   on_attach = on_attach,
   cmd = {"gopls", "serve"},
   filetypes = { "go", "gomod" },
-  root_dir = nvim_lsp.util.root_pattern("go.mod", ".git"),
+  root_dir = nvim_lsp.util.root_pattern(".git", "go.mod"),
   settings = {
     gopls = {
       analyses = {
