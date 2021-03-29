26dda84d6c3a96c76d448b3ec4595117067e6384
hienduyph <hienduyph@gmail.com>
2021-03-29 18:09:04 +0700

    vim: gopls disable static check

--- vim/neovim/lua/lsp.lua
+++ vim/neovim/lua/lsp.lua
@@ -65,7 +65,7 @@ nvim_lsp.gopls.setup {
       analyses = {
         unusedparams = true,
       },
-      staticcheck = true,
+      staticcheck = false,
       experimentalWorkspaceModule = true,
     },
   },
