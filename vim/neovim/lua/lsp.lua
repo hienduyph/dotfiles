9a40b0fd688a3a93257022167f0e5a260433f9c9
hienduyph <hienduyph@gmail.com>
2021-04-16 13:52:05 +0700

    vim: use ale for code linter

--- vim/neovim/lua/lsp.lua
+++ vim/neovim/lua/lsp.lua
@@ -40,7 +40,7 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
   buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
   buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  -- buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
   buf_set_keymap('n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
   buf_set_keymap('n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
   buf_set_keymap('n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
@@ -89,7 +89,7 @@ nvim_lsp.gopls.setup {
   settings = {
     gopls = {
       analyses = {
-        unusedparams = true,
+        unusedparams = false,
       },
       buildFlags={"-tags=integration wireinject unit"},
       staticcheck = false,
