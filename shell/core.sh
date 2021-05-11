15d2b867a7aad8e477eff344288eb5a43c98d932
Hien Pham <hienduyph@gmail.com>
2021-05-11 22:11:36 +0700

    shell: core py setup

--- shell/core.sh
+++ shell/core.sh
@@ -30,3 +30,8 @@ fi
 export FZF_DEFAULT_COMMAND='rg --files --hidden --no-ignore --follow -g "!{.git,node_modules,vendor,.direnv,.mypy_cache,__pycache__,target,.pytest_cache,.next}" 2> /dev/null'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_ALT_C_COMMAND="cd ~/; bfs -type d -nohidden | sed s/^\./~/"
+
+
+py_core_setup() {
+  /usr/bin/env python -m pip install black pylint ipdb wheel pynvim --upgrade
+}
