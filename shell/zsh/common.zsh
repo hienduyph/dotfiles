31d565720576d7a6dfbfba9a486d8d7bb8a1ca8d
Hien Pham <hienduyph@gmail.com>
2021-05-17 17:31:02 +0700

    vim: try to add vimspector

--- shell/zsh/common.zsh
+++ shell/zsh/common.zsh
@@ -53,13 +53,6 @@ eval "$(direnv hook zsh)"
 # direnv support venv
 setopt PROMPT_SUBST
 
-show_virtual_env() {
-  if [[ -n "$VIRTUAL_ENV" && -n "$DIRENV_DIR" ]]; then
-    echo "($(basename $VIRTUAL_ENV))"
-  fi
-}
-PS1='$(show_virtual_env)'$PS1
-
 alias k="kubectl"
 
 # Check if 'kubectl' is a command in $PATH
