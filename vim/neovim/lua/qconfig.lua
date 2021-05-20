2ea999aa633e71323dac5bddd1f2baf4eb603935
Hien PHAM <hienduyph@gmail.com>
2021-05-21 05:49:20 +0700

    alacritty: mac

--- vim/neovim/lua/qconfig.lua
+++ vim/neovim/lua/qconfig.lua
@@ -1,5 +1,13 @@
 require('gitsigns').setup {
-  use_internal_diff = false,
+  watch_index = {
+    interval = 1000
+  },
+  current_line_blame = false,
+  sign_priority = 6,
+  update_debounce = 100,
+  status_formatter = nil, -- Use default
+  use_decoration_api = true,
+  use_internal_diff = true,
 }
 
 -- use tree sitter
