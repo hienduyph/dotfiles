6dbb9a8290acb58bfeaf62236331d8980d6b44fb
Hien Pham <hienduyph@gmail.com>
2021-05-23 22:50:54 +0700

    fedora: add kvm

---
+++ platform/fedora/kvm.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+sudo dnf group install --with-optional virtualization -y
+sudo dnf install libvirt-devel libguestfs-tools-c libvirt libxml2-devel libxslt-devel flex bison  -y
+sudo systemctl start libvirtd
+sudo systemctl enable libvirtd
