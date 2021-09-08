fdcd4084c265690edf262542fc9b87ac0b84d6c4
Hien Pham <hienduyph@gmail.com>
2021-09-08 13:09:24 +0700

    vim: telescope for nvim0.5

--- vim/neovim/lua/qconfig.lua
+++ vim/neovim/lua/qconfig.lua
@@ -1,4 +1,4 @@
-require('gitsigns').setup()
+-- require('gitsigns').setup()
 
 -- use tree sitter
 require'nvim-treesitter.configs'.setup {
@@ -14,3 +14,20 @@ require'nvim-treesitter.configs'.setup {
     additional_vim_regex_highlighting = false,
   },
 }
+
+require'nvim-web-devicons'.setup {
+}
+require('telescope').setup {
+  extensions = {
+    fzf = {
+      fuzzy = true,                    -- false will only do exact matching
+      override_generic_sorter = false, -- override the generic sorter
+      override_file_sorter = true,     -- override the file sorter
+      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                       -- the default case_mode is "smart_case"
+    }
+  }
+}
+-- To get fzf loaded and working with telescope, you need to call
+-- load_extension, somewhere after setup function:
+require('telescope').load_extension('fzf')
