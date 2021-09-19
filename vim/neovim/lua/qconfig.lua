848503f2368bc84dae824be04a605335e1eca515
Hien Pham <hienduyph@gmail.com>
2021-09-19 18:36:39 +0700

    vim: goodbye pandoc

--- vim/neovim/lua/qconfig.lua
+++ vim/neovim/lua/qconfig.lua
@@ -11,6 +11,16 @@ npairs.setup({
   }
 })
 
+local parser_config = require'nvim-treesitter.parsers'.get_parser_configs()
+parser_config.gotmpl = {
+  install_info = {
+    url = "https://github.com/ngalaiko/tree-sitter-go-template",
+    files = {"src/parser.c"}
+  },
+  filetype = "gotmpl",
+  used_by = {"gohtmltmpl", "gotexttmpl", "gotmpl", "yaml"}
+}
+
 -- use tree sitter
 require'nvim-treesitter.configs'.setup {
   ensure_installed = "maintained",
