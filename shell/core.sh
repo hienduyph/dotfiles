802fccb596b374571cb11142440ce15e2f39f607
Hien Pham <hienduyph@gmail.com>
2021-10-12 09:35:25 +0700

    fzf: fd command

--- shell/core.sh
+++ shell/core.sh
@@ -27,9 +27,9 @@ if [[ -f "${LINUX_BREW}/bin/brew" ]]; then
   addToPATH "${LINUX_BREW}/sbin"
 fi
 
-export FZF_DEFAULT_COMMAND='rg --files --hidden --no-ignore --follow -g "!{.git,node_modules,vendor,.direnv,.mypy_cache,__pycache__,target,.pytest_cache,.next}" 2> /dev/null'
+export FZF_DEFAULT_COMMAND="fd --type f --hidden --follow --exclude '.git|node_modules|vendor|.direnv|.mypy_cache|__pycache__|target|.pytest_cache|.next'"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export FZF_ALT_C_COMMAND="cd ~/; bfs -type d -nohidden | sed s/^\./~/"
+export FZF_ALT_C_COMMAND="fd -t d"
 
 
 py_core_setup() {
