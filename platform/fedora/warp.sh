05f3e9e946e609c9a4088bad19bd45990ea49730
Hien Pham <hienduyph@gmail.com>
2022-02-10 15:48:50 +0700

    warp: install with custom repo

--- platform/fedora/warp.sh
+++ platform/fedora/warp.sh
@@ -2,7 +2,18 @@
 
 set -e
 
-sudo rpm -ivh http://pkg.cloudflareclient.com/cloudflare-release-el8.rpm
+# sudo rpm -ivh http://pkg.cloudflareclient.com/cloudflare-release-el8.rpm
+
+cat <<EOF | sudo tee /etc/yum.repos.d/cloudflare.repo
+[cloudflare]
+name=Cloudflare CentOS Packages
+baseurl=https://pkg.cloudflareclient.com/dists/8/main/binary-\$basearch
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CLOUDFLARE-3
+EOF
+
 sudo dnf install cloudflare-warp -y
 sudo systemctl enable warp-svc
 sudo systemctl restart warp-svc
