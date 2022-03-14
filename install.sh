a77b3a39d0c444d627394a5d870142c1af072ff5
Hien Pham <hienduyph@gmail.com>
2022-03-14 18:28:31 +0700

    new mac

--- install.sh
+++ install.sh
@@ -6,14 +6,19 @@ source ./scripts/dotfiles.sh
 PLATFORM="$(uname -s | tr '[:upper:]' '[:lower:]')"
 
 _cli() {
-  curl -fsSL https://github.com/mozilla/sccache/releases/download/v0.2.15/sccache-v0.2.15-x86_64-unknown-linux-musl.tar.gz | sudo tar zx -C /usr/local/bin --strip-components=1
-  sudo chmod +x /usr/local/bin/sccache
+  if ! command -v sccache &> /dev/null
+  then
+    curl -fsSL https://github.com/mozilla/sccache/releases/download/v0.2.15/sccache-v0.2.15-x86_64-unknown-linux-musl.tar.gz | sudo tar zx -C /usr/local/bin --strip-components=1
+    sudo chmod +x /usr/local/bin/sccache
+  fi
+
   source $HOME/.cargo/env
   cargo build --release
   cp target/release/json2yaml target/release/yaml2json ~/.local/bin
 }
 
 _system() {
+  sudo mkdir -p /etc/profile.d/
   sudo cp $HOME/dotfiles/shell/system.sh /etc/profile.d/
 }
 
@@ -21,6 +26,7 @@ main() {
   _neovim
   _shell
   _dots $PLATFORM
+  _fonts
   _system
   _completions
   _cli
