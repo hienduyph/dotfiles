7ba914e414996ecbc7dbcf2e1d4c6f2a11be8b44
Hien Pham <hienduyph@gmail.com>
2022-03-26 14:34:35 +0700

    cli: yaml & json converter

--- shell/core.sh
+++ shell/core.sh
@@ -27,8 +27,20 @@ export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_ALT_C_COMMAND="fd -t d"
 
 py_core_setup() {
-  /usr/bin/env python -m pip install black pylint ipdb wheel pynvim --upgrade
+  /usr/bin/env python -m pip install black pylint ipdb wheel pynvim pip --upgrade
 }
+
+if ! command -v json2yaml >/dev/null ; then
+  json2yaml() {
+    ${PYCLI_HOME}/bin/python -c 'import sys, yaml, json; sys.stdout.write(yaml.dump(json.load(sys.stdin)))'
+  };
+fi
+
+if ! command -v yaml2json >/dev/null ; then
+  yaml2json() {
+    ${PYCLI_HOME}/bin/python -c 'import sys, yaml, json; sys.stdout.write(yaml.dump(json.load(sys.stdin)))'
+  };
+fi
 
 # https://github.com/cheat/cheat
 export CHEAT_USE_FZF=true
