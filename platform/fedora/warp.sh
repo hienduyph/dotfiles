6aca5465337149067277f6df94cff22179a14f34
Hien Pham <hienduyph@gmail.com>
2022-05-25 13:55:26 +0700

    fedora: install cloudflare warp

--- platform/fedora/warp.sh
+++ platform/fedora/warp.sh
@@ -4,19 +4,8 @@ set -e
 
 # sudo rpm -ivh http://pkg.cloudflareclient.com/cloudflare-release-el8.rpm
 
-cat <<EOF | sudo tee /etc/yum.repos.d/cloudflare.repo
-[cloudflare]
-name=Cloudflare CentOS Packages
-baseurl=https://pkg.cloudflareclient.com/dists/8/main/binary-\$basearch
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CLOUDFLARE-3
-EOF
-
-sudo dnf install cloudflare-warp -y
-sudo systemctl enable warp-svc
-sudo systemctl restart warp-svc
+sudo dnf install -y https://pkg.cloudflareclient.com/uploads/cloudflare_warp_2022_4_235_1_x86_64_aa859896da.rpm
+sudo systemctl enable --now warp-svc
 
 warp-cli register
 warp-cli set-mode proxy
