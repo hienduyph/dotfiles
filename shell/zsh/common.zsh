b52cb8b3125d51f6f4c101758918a4bcfbc50a33
Hien Pham <hien.pham2@tiki.vn>
2022-06-23 14:23:49 +0700

    zsh: clean some things

--- shell/zsh/common.zsh
+++ shell/zsh/common.zsh
@@ -30,18 +30,5 @@ if [[ -f ${BREW_PREFIX}/bin/brew ]]; then
 fi
 export FPATH=$FPATH:${CF_PATH}
 
-# autoload -Uz compinit && compinit
+# znap already set completion for us.
 # https://medium.com/@dannysmith/little-thing-2-speeding-up-zsh-f1860390f92
-autoload -Uz compinit
-for dump in ~/.zcompdump(N.mh+24); do
-  compinit
-done
-compinit -C
-
-autoload -U +X bashcompinit && bashcompinit
-
-## 3third plugins
-eval "$(direnv hook zsh)"
-
-# direnv support venv
-setopt PROMPT_SUBST
