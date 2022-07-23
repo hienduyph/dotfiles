fe51bc55f2537c3a8cf7824d3dd8159b58ad7141
Hien Pham <hienduyph@gmail.com>
2022-07-23 09:54:55 +0700

    fedora: warp & math stuff

--- platform/fedora/warp.sh
+++ platform/fedora/warp.sh
@@ -2,9 +2,21 @@
 
 set -e
 
-# sudo rpm -ivh http://pkg.cloudflareclient.com/cloudflare-release-el8.rpm
+sudo rpm -ivh http://pkg.cloudflareclient.com/cloudflare-release-el8.rpm
 
-sudo dnf install -y https://pkg.cloudflareclient.com/uploads/cloudflare_warp_2022_4_235_1_x86_64_aa859896da.rpm
+# fix releasever for fedora
+sudo tee /etc/yum.repos.d/cloudflare.repo << EOM
+[cloudflare]
+name=Cloudflare CentOS Packages
+baseurl=https://pkg.cloudflareclient.com/dists/8/main/binary-\$basearch
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CLOUDFLARE-3
+EOM
+
+sudo dnf install cloudflare-warp -y
+
 sudo systemctl enable --now warp-svc
 
 warp-cli register
