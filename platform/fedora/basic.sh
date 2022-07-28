4cc3aba787bac784aacace64f43bb6178502e52b
Hien Pham <hienduyph@gmail.com>
2022-07-28 21:52:55 +0700

    fedora: update stuff

--- platform/fedora/basic.sh
+++ platform/fedora/basic.sh
@@ -13,7 +13,7 @@ _pkg config-manager --add-repo  https://yum.enpass.io/enpass-yum.repo
 
 
 echo "Add ibus"
-_pkg config-manager --add-repo https://download.opensuse.org/repositories/home:lamlng/Fedora_35/home:lamlng.repo
+_pkg config-manager --add-repo https://download.opensuse.org/repositories/home:lamlng/Fedora_36/home:lamlng.repo
 
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc && \
   sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
@@ -31,7 +31,7 @@ _pkg install -y https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-r
 
 echo "Add albert"
 sudo rpm --import https://build.opensuse.org/projects/home:manuelschneid3r/public_key
-_pkg config-manager --add-repo https://download.opensuse.org/repositories/home:manuelschneid3r/Fedora_35/home:manuelschneid3r.repo
+_pkg config-manager --add-repo https://download.opensuse.org/repositories/home:manuelschneid3r/Fedora_36/home:manuelschneid3r.repo
 
 _pkg copr enable robot/rust-analyzer -y
 
--- shell/core.sh
+++ shell/core.sh
@@ -52,9 +52,6 @@ export CHEAT_USE_FZF=true
 
 export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 
-# source secrets files
-export PROFILE_EXTRA_DIRS=$HOME/.profile_src
-
 if [[ -d $PROFILE_EXTRA_DIRS ]]; then
   for filename in $PROFILE_EXTRA_DIRS/*.sh; do
     source ${filename}
