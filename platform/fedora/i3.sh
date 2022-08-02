7a1c7ba2fd44dc8b5c5b8a6ea94a34197283f274
Hien Pham <hienduyph@gmail.com>
2022-08-02 19:22:56 +0700

    move i3 to fedora installer

---
+++ platform/fedora/i3.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo dnf install i3 xset redshift wlr-randr dex-autostart rofi -y
+
--- platform/linux/lightdm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo dnf install -y lightdm
-sudo systemctl disable gdm
-sudo systemctl enable lightdm
-
