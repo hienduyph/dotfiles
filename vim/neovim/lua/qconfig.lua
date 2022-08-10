23558de40058e117e76607013c63a7b0378ffdc2
Hien Pham <hienduyph@gmail.com>
2022-08-11 06:29:19 +0700

    vim: colro setup

--- vim/neovim/lua/qconfig.lua
+++ vim/neovim/lua/qconfig.lua
@@ -1,149 +1,22 @@
-vim.diagnostic.config({
-  virtual_text = false
-})
-
-require('gitsigns').setup()
-
-local npairs = require("nvim-autopairs")
-
-npairs.setup({
-  check_ts = true,
-  ts_config = {
-    lua = {'string'},-- it will not add a pair on that treesitter node
-    javascript = {'template_string'},
-    java = false,-- don't check treesitter on java
-  }
-})
-
-local parser_config = require'nvim-treesitter.parsers'.get_parser_configs()
-parser_config.gotmpl = {
-  install_info = {
-    url = "https://github.com/ngalaiko/tree-sitter-go-template",
-    files = {"src/parser.c"}
-  },
-  filetype = "gotmpl",
-  used_by = {"gohtmltmpl", "gotexttmpl", "gotmpl", "yaml"}
-}
-
--- use tree sitter
-require'nvim-treesitter.configs'.setup {
-  ignore_install = {"norg"},
-  autopairs = {
-    enable = true,
-  },
-  indent = {
-    enable = true,
-    -- workaround because python indent is bad!
-    disable = {"python", "yaml", "solidity", "go"},
-  },
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-}
-
-require'nvim-web-devicons'.setup {
-}
-
-require('kommentary.config').use_extended_mappings()
-
-
-vim.g.indent_blankline_filetype_exclude = {'dashboard'}
-vim.g.indent_blankline_buftype_exclude = {'dashboard', 'terminal'}
-
-require("indent_blankline").setup {
-  char = "|",
-  buftype_exclude = {"terminal"},
-  space_char_blankline=' ',
-  use_treesitter=true,
-  char_list={'|', '¦', '┆', '┊'},
-}
-
-require("bufferline").setup{
-  options = {
-    numbers = "ordinal",
-    diagnostics = "nvim_lsp",
-    show_buffer_close_icons = false,
-    show_close_icon = false,
-  },
-}
-
-local navic = require("nvim-navic")
-
-require('lualine').setup({
-  options = {
-    theme = 'gruvbox',
-    always_show_bufferline=true,
-  },
-  extensions = {'quickfix', 'nvim-tree', 'fzf'},
-  sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch'},
-    lualine_c = {
-      {
-        'filename',
-        file_status=true,
-        path=1,
-      },
-      {
-        navic.get_location,
-        cond = navic.is_available,
-      },
-    },
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress', 'diff'},
-    lualine_z = {'location'}
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {
-  },
-})
-
-require'nvim-tree'.setup {
-  update_focused_file = {
-    enable = true,
-  },
-  filters = {
-    dotfiles = false,
-    custom = {  '.git', 'node_modules', '.cache', '__pycache__', '.direnv', '.ipynb_checkpoints', 'vendor' },
-  },
-  actions = {
-    open_file = {
-      resize_window = true,
-      window_picker = {
-        enable = false,
-      },
-    },
-  },
-}
-
-require('rust-tools').setup({})
-
-require('mini.base16').setup({
+-- custom fonts
+require("mini.base16").setup({
   palette = {
-    base00 = '#ffffff',
-    base01 = '#f6f8eb',
-    base02 = '#dbe1ac',
-    base03 = '#73765a',
-    base04 = '#324490',
-    base05 = '#002a83',
-    base06 = '#0000e4',
-    base07 = '#080500',
-    base08 = '#5e2200',
-    base09 = '#a86400',
-    base0A = '#008818',
-    base0B = '#004500',
-    base0C = '#b34aad',
-    base0D = '#004b76',
-    base0E = '#7d0077',
-    base0F = '#0086ae',
+    base00 = "#ffffff",
+    base01 = "#f6f8eb",
+    base02 = "#dbe1ac",
+    base03 = "#73765a",
+    base04 = "#324490",
+    base05 = "#002a83",
+    base06 = "#0000e4",
+    base07 = "#080500",
+    base08 = "#5e2200",
+    base09 = "#a86400",
+    base0A = "#008818",
+    base0B = "#004500",
+    base0C = "#b34aad",
+    base0D = "#004b76",
+    base0E = "#7d0077",
+    base0F = "#0086ae",
   },
   use_cterm = {
     base00 = 254,
@@ -164,3 +37,132 @@ require('mini.base16').setup({
     base0F = 31,
   },
 });
+
+vim.diagnostic.config({
+  virtual_text = false
+})
+
+require("gitsigns").setup()
+
+local npairs = require("nvim-autopairs")
+
+npairs.setup({
+  check_ts = true,
+  ts_config = {
+    lua = {"string"},-- it will not add a pair on that treesitter node
+    javascript = {"template_string"},
+    java = false,-- don"t check treesitter on java
+  }
+})
+
+local parser_config = require"nvim-treesitter.parsers".get_parser_configs()
+parser_config.gotmpl = {
+  install_info = {
+    url = "https://github.com/ngalaiko/tree-sitter-go-template",
+    files = {"src/parser.c"}
+  },
+  filetype = "gotmpl",
+  used_by = {"gohtmltmpl", "gotexttmpl", "gotmpl", "yaml"}
+}
+
+-- use tree sitter
+require"nvim-treesitter.configs".setup {
+  ignore_install = {"norg"},
+  autopairs = {
+    enable = true,
+  },
+  indent = {
+    enable = true,
+    -- workaround because python indent is bad!
+    disable = {"python", "yaml", "solidity", "go"},
+  },
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+}
+
+require"nvim-web-devicons".setup {
+}
+
+require("kommentary.config").use_extended_mappings()
+
+require("indent_blankline").setup {
+  char = "|",
+  buftype_exclude = {"terminal", "dashboard"},
+  space_char_blankline=" ",
+  use_treesitter=true,
+  char_list={"|", "¦", "┆", "┊"},
+  show_current_context = true,
+  show_current_context_start = true,
+}
+vim.cmd[[highlight IndentBlanklineChar guifg=#dedede gui=nocombine]]
+vim.cmd[[highlight IndentBlanklineContextChar guifg=#cfcccc gui=nocombine]]
+
+require("bufferline").setup{
+  options = {
+    numbers = "ordinal",
+    diagnostics = "nvim_lsp",
+    show_buffer_close_icons = false,
+    show_close_icon = false,
+  },
+}
+
+local navic = require("nvim-navic")
+
+require("lualine").setup({
+  options = {
+    theme = "gruvbox",
+    always_show_bufferline=true,
+  },
+  extensions = {"quickfix", "nvim-tree", "fzf"},
+  sections = {
+    lualine_a = {"mode"},
+    lualine_b = {"branch"},
+    lualine_c = {
+      {
+        "filename",
+        file_status=true,
+        path=1,
+      },
+      {
+        navic.get_location,
+        cond = navic.is_available,
+      },
+    },
+    lualine_x = {"encoding", "fileformat", "filetype"},
+    lualine_y = {"progress", "diff"},
+    lualine_z = {"location"}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {"filename"},
+    lualine_x = {"location"},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {
+  },
+})
+
+require"nvim-tree".setup {
+  update_focused_file = {
+    enable = true,
+  },
+  filters = {
+    dotfiles = false,
+    custom = {  ".git", "node_modules", ".cache", "__pycache__", ".direnv", ".ipynb_checkpoints", "vendor" },
+  },
+  actions = {
+    open_file = {
+      resize_window = true,
+      window_picker = {
+        enable = false,
+      },
+    },
+  },
+}
+
+require("rust-tools").setup({})
+
