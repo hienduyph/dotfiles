049320fd9f41aa0b24d255ab808d3c33382fdc4f
Hien Pham <hien.pham2@tiki.vn>
2022-08-17 14:49:29 +0700

    fcitx5: tweak

--- .config/nvim/lua/q/plugin.lua
+++ .config/nvim/lua/q/plugin.lua
@@ -117,7 +117,6 @@ return require('packer').startup(function(use)
       local null_ls = require("null-ls")
       null_ls.setup({
         sources = {
-          null_ls.builtins.formatting.stylua,
           null_ls.builtins.diagnostics.eslint_d,
           null_ls.builtins.diagnostics.buf,
           null_ls.builtins.formatting.black,
