a6e5904864f154632d4ddb23c46ba683c8edcd86
Hien Pham <hienduyph@gmail.com>
2022-08-17 20:06:59 +0700

    shell: move cores funcs to common

--- platform/fedora/upgrade.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -ex
-
-sudo dnf update -y && sudo dnf upgrade -y && sudo dnf autoremove -y
--- shell/func.sh
+++ shell/func.sh
@@ -48,3 +48,77 @@ gh_latest_release() {
 _pkg() {
 	sudo dnf "$@"
 }
+
+_linux_telegram() {
+  DST=$1
+  DST=${DST:-/opt/Telegram}
+  VERSION="$(gh_latest_release telegramdesktop/tdesktop)"
+  VERSION=${VERSION#v}
+  sudo mkdir -p $DST
+  curl -fSL -o /tmp/file.gz "https://github.com/telegramdesktop/tdesktop/releases/download/v${VERSION}/tsetup.${VERSION}.tar.xz"
+  sudo tar xf /tmp/file.gz -C $DST --strip-components=1
+}
+
+_fedora_wezterm() {
+  wezterm_ver="$(gh_latest_release wez/wezterm)"
+  fname="$(echo $wezterm_ver | sed 's/-/_/g')"
+  echo "Install WezTerm ${wezterm_ver}"
+  sudo dnf install -y "https://github.com/wez/wezterm/releases/download/${wezterm_ver}/wezterm-${fname}-1.fedora36.x86_64.rpm"
+}
+
+_github_pks() {(
+  set -ex
+  luals="$(gh_latest_release sumneko/lua-language-server)"
+  sudo mkdir -p /opt/LuaLS
+  curl -fSsL "https://github.com/sumneko/lua-language-server/releases/download/${luals}/lua-language-server-${luals}-linux-x64.tar.gz" | sudo tar xz -C /opt/LuaLS
+  sudo ln -sf /opt/LuaLS/bin/lua-language-server /usr/local/bin/
+
+  sudo curl -fssLo /usr/local/bin/yq "https://github.com/mikefarah/yq/releases/download/$(gh_latest_release mikefarah/yq)/yq_linux_amd64"
+  sudo chmod +x /usr/local/bin/yq
+)}
+
+_linux_nvim_nightly() { (set -e
+  echo "update nvim nightly"
+  TMP_DIR=/tmp/neovim
+  rm -rf $TMP_DIR && mkdir -p ${TMP_DIR}
+  echo "Downloading"
+  curl -fsSL https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz | tar xz -C $TMP_DIR --strip-components=1
+  DST_DIR=/opt/neovim-nightly
+  if [ ! -d $DST_DIR ]; then
+    echo "Creating nvim nightly folder"
+    sudo mkdir -p ${DST_DIR}
+    sudo chown -R $(whoami) ${DST_DIR}
+  fi
+  echo "Extrating"
+  sudo rm -rf ${DST_DIR}/*
+  sudo mv $TMP_DIR/* ${DST_DIR}/
+  sudo ln -sf ${DST_DIR}/bin/nvim /usr/local/bin/vv
+  echo "Done!"
+)}
+
+_linux_nvim() { (set -e
+  VERSION=$(gh_latest_release neovim/neovim)
+  echo "Install latest stable nvim: ${VERSION}"
+  TMP_DIR=/tmp/neovim
+  rm -rf $TMP_DIR && mkdir -p ${TMP_DIR}
+  echo "Downloading"
+  curl -fsSL "https://github.com/neovim/neovim/releases/download/${VERSION}/nvim-linux64.tar.gz" | tar xz -C $TMP_DIR --strip-components=1
+  DST_DIR=/opt/neovim
+  if [ ! -d ${DST_DIR} ]; then
+    echo "Create folder first"
+    sudo mkdir -p ${DST_DIR}
+    sudo chown -R $(whoami) ${DST_DIR}
+  else
+    sudo rm -rf "${DST_DIR}/*" || true
+  fi
+
+  echo "Extrating to ${DST_DIR}"
+  sudo mv $TMP_DIR/* ${DST_DIR}/
+  sudo ln -sf ${DST_DIR}/bin/nvim /usr/local/bin/nvim
+  sudo ln -sf ${DST_DIR}/bin/nvim /usr/local/bin/vim
+  echo "Done!"
+)}
+
+_fedora_upgrade() {
+  sudo dnf update -y && sudo dnf upgrade -y && sudo dnf autoremove -y
+}
