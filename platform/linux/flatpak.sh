a15ac1c5933aa0bf958620b7a87ff5209e373d3a
Hien Pham <hienduyph@gmail.com>
2022-09-20 17:07:05 +0700

    flatpak: add telegram

--- platform/linux/flatpak.sh
+++ platform/linux/flatpak.sh
@@ -10,6 +10,7 @@ flatpaks=(
   net.cozic.joplin_desktop
   com.jetbrains.IntelliJ-IDEA-Community
   com.google.AndroidStudio
+  org.telegram.desktop
 )
 for pkg in "${flatpaks[@]}";do
   flatpak install flathub ${pkg} -y
