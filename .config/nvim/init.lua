2c6eb2fd97306ea8ab068f6107f1816412a6c141
Hien Pham <hienduyph@gmail.com>
2022-09-25 09:56:09 +0700

    nvim: use fzf lua with swticher

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,14 @@
+vim.g.fzf = true
+
 require("q.plugin")
 require("q.01init")
 require("q.config")
 require("q.cmd")
 require("q.keymap")
-require('q.telescope')
+
+if vim.g.fzf == False then
+  require("q.telescope")
+end
+
 require("q.lsp")
-require('q.completion')
+require("q.completion")
