506309fe22027b256c2060e64ec6593cba76e557
Hien Pham <hien.pham2@tiki.vn>
2022-10-11 11:11:34 +0700

    apps: local joplin apps

--- shell/system/linux_common.sh
+++ shell/system/linux_common.sh
@@ -1,3 +1,27 @@
+joplin_install() {{
+  set -e
+  VERSION="$(gh_latest_release laurent22/joplin)"
+  VERSION=${VERSION#v}
+  echo "Joplin Install Version ${VERSION}"
+  DST=$APP_HOME/Joplin.AppImage
+  curl -fL -o $DST "https://github.com/laurent22/joplin/releases/download/v${VERSION}/Joplin-${VERSION}.AppImage"
+  chmod +x $DST
+  curl -Lo $HOME/.local/share/icons/joplin.png 'https://raw.githubusercontent.com/laurent22/joplin/dev/Assets/LinuxIcons/1024x1024.png'
+
+  tee $HOME/.local/share/applications/joplin.desktop << EOM
+[Desktop Entry]
+Version=1.0
+Name=Joplin
+Comment=Secured Notes
+Exec=$DST %u
+Icon=joplin
+Type=Application
+StartupWMClass=Joplin
+EOM
+
+  echo "Done!!"
+}}
+
 u() {
   if command -v rustup > /dev/null; then
     rustup self update
