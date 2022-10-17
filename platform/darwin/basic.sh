779986a86c1f578e06d1e9a89a359d0da9263176
Hien Pham <hien.pham2@tiki.vn>
2022-10-17 15:43:01 +0700

    mac: homebrew for local pkgs

--- platform/darwin/basic.sh
+++ platform/darwin/basic.sh
@@ -1,47 +1,6 @@
 #!/bin/bash
-set -e
 
-PKGS=(
-  zoxide
-  fzf
-  exa
-  bat
-  neovim
-  tmux
-  tmux-pasteboard
-  direnv
-  starship
-  alacritty
-  git-delta
-  gnupg2
-  git
-  git-lfs
-  python39
-  nodejs16
-  go
-  fd
-  ripgrep
-  inetutils
-  wget
-  jq
-  gsed
-  findutils
-  rust-analyzer
-  rust
-  cargo
-  openjdk11
-  podman
-  podman-compose
-  htop
-  google-cloud-sdk
-  sccache
-  OpenBLAS lapack
-  mysql8
-  postgresql14
-  yq
-  lua-language-server
-)
-sudo port -N install ${PKGS[@]}
-sudo port select mysql mysql8
+set -ex
 
-curl -qL https://www.npmjs.com/install.sh | sh
+curl -s "https://get.sdkman.io" | bash
+sdk install java 11.0.16.1-tem
