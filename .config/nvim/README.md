b42fd64e14f69a20d91759b58b8e26e494ee2816
Hien Pham <hien.pham2@tiki.vn>
2022-10-28 14:02:39 +0700

    chores: update some notes

--- .config/nvim/README.md
+++ .config/nvim/README.md
@@ -3,3 +3,5 @@
 ## Requires
 - git: for packer to work
 - `C` & `C++` compiler: for nvim-treesitter compiles grammar.
+- node & npm: needed for some language-server to install.
+- go: to install gopls & work with go project
--- .config/nvim/lua/q/02plugin.lua
+++ .config/nvim/lua/q/02plugin.lua
@@ -19,6 +19,7 @@ return require('packer').startup(function(use)
     run = function() require("nvim-treesitter.install").update { with_sync = true } end,
   }
   use { 'echasnovski/mini.nvim', branch = 'stable' }
+  use {'numToStr/Comment.nvim', config = function() require('Comment').setup() end }
   use 'lewis6991/gitsigns.nvim'
   use "sindrets/diffview.nvim"
   use "kyazdani42/nvim-tree.lua"
