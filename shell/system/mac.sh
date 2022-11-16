2aadc1514d08293c4bd67c3f8ee317251f6b2330
Hien Pham <hien.pham2@tiki.vn>
2022-11-16 13:30:10 +0700

    mac: nvim update

--- shell/system/mac.sh
+++ shell/system/mac.sh
@@ -2,4 +2,6 @@ export HOMEBREW_CASK_OPTS="--appdir=~/Applications"
 
 u() {
   brew update && brew upgrade && brew cleanup
+  nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+  nvim --headless -c 'TSUpdateSync' -c 'quitall'
 }
