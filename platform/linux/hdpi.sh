bcb7c4061f231a2b5efaa32fa917aafb7ec5f06d
Hien Pham <hienduyph@gmail.com>
2022-11-21 07:07:36 +0700

    linux: tweak stuff

---
+++ platform/linux/hdpi.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+sudo tee /usr/share/glib-2.0/schemas/99_hidpi.gschema.override << EOF
+[org.gnome.desktop.interface]
+scaling-factor=1
+text-scaling-factor=1.5
+EOF
+sudo glib-compile-schemas /usr/share/glib-2.0/schemas
