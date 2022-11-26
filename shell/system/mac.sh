472e0b11db8df0a14516ceda27b71d04adb0e469
Hien Pham <hienduyph@gmail.com>
2022-11-26 07:31:51 +0700

    mac: tweak stuff

--- shell/system/mac.sh
+++ shell/system/mac.sh
@@ -5,3 +5,23 @@ u() {
   nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
   nvim --headless -c 'TSUpdateSync' -c 'quitall'
 }
+
+xcode_dl() {
+  # https://developer.apple.com/download/applications/
+  local VERSION=$1
+  VERSION=${VERSION:-14.1}
+  local URL="https://download.developer.apple.com/Developer_Tools/Xcode_${VERSION}/Xcode_${VERSION}.xip"
+
+  local TOKEN=$APPLE_AUTH_TOKEN
+
+  aria2c --header "Host: adcdownload.apple.com" \
+    --header "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"\
+    --header "Upgrade-Insecure-Requests: 1"\
+    --header "Cookie: ADCDownloadAuth=$TOKEN"\
+    --header "User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 10_1 like Mac OS X) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0 Mobile/14B72 Safari/602.1" \
+    --header "Accept-Language: en-us" -x 16 -s 16 \
+    ${URL} \
+    -d ~/Downloads
+
+  xip -x ~/Downloads/Xcode_${VERSION}
+}
