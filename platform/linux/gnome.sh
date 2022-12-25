101c8b9fe427d4ba1f58f31b526966090a20664d
Hien Pham <hienduyph@gmail.com>
2022-12-25 20:25:36 +0700

    split gnome conf

--- platform/linux/gnome.sh
+++ platform/linux/gnome.sh
@@ -1,54 +1,35 @@
-#!/bin/bash
-# auto install extensions
-# https://unix.stackexchange.com/questions/617288/command-line-tool-to-install-gnome-shell-extensions
+#!/usr/bin/env bash
 APP_ROOT="$(dirname "$(readlink -fm "$0")")"
 
-_theme() {
-  local workdir=/tmp/theme
-  mkdir $workdir
-  git clone https://github.com/vinceliuice/WhiteSur-icon-theme.git $workdir
-  $workdir/install.sh -t default -a -b --black
-  rm -rf $workdir
-}
-
-exts=(
-  https://extensions.gnome.org/extension/1401/bluetooth-quick-connect/
-  https://extensions.gnome.org/extension/307/dash-to-dock/
-  https://extensions.gnome.org/extension/4451/logo-menu/
-  https://extensions.gnome.org/extension/1287/unite/
-  https://extensions.gnome.org/extension/19/user-themes/
-  https://extensions.gnome.org/extension/841/freon/
-  https://extensions.gnome.org/extension/28/gtile/
-  https://extensions.gnome.org/extension/5237/rounded-window-corners/
+keys=(
+  /org/gnome/shell/
+  /org/gnome/desktop/
+  /org/gnome/mutter/
+  /org/gnome/settings-daemon/
+)
+files=(
+  $APP_ROOT/dconf/shell.dconf
+  $APP_ROOT/dconf/desktop.dconf
+  $APP_ROOT/dconf/mutter.dconf
+  $APP_ROOT/dconf/settings.dconf
 )
 
-_install_ext() {
-  URL=$1
-  EXTENSION_ID=$(curl -s $URL| grep -oP 'data-uuid="\K[^"]+')
-  echo "Install ${URL} with id ${EXTENSION_ID}"
-  VERSION_TAG=$(curl -Lfs "https://extensions.gnome.org/extension-query/?search=$EXTENSION_ID" | jq '.extensions[0] | .shell_version_map | map(.pk) | max')
-  wget -O ${EXTENSION_ID}.zip "https://extensions.gnome.org/download-extension/${EXTENSION_ID}.shell-extension.zip?version_tag=$VERSION_TAG"
-  gnome-extensions install --force -q ${EXTENSION_ID}.zip
-  rm ${EXTENSION_ID}.zip
+_load() {
+  for i in "${!keys[@]}"; do 
+    printf "Load \t%s\n" "${files[$i]}" "${files[$i]}"
+    dconf load "${keys[$i]}" < "${files[$i]}"
+  done
 }
 
-_enable() {
-  EXTENSION_ID=$1
-  echo "Waiting user accept!!"
-  if ! gnome-extensions list | grep --quiet ${EXTENSION_ID}; then
-    busctl --user call org.gnome.Shell.Extensions /org/gnome/Shell/Extensions org.gnome.Shell.Extensions InstallRemoteExtension s ${EXTENSION_ID}
-  fi
-  gnome-extensions enable ${EXTENSION_ID}
+_dump() {
+  for i in "${!keys[@]}"; do 
+    printf "Dump \t%s\n" "${files[$i]}" "${files[$i]}"
+    dconf dump "${keys[$i]}" > "${files[$i]}"
+  done
 }
 
-echo "Installing Extensions"
-for url in "${exts[@]}"; do
-  _install_ext $url
-done
-
-_theme
-
-
-echo "Restore configs"
-dconf load / < $APP_ROOT/dconf/gnome.dconf
-dconf load /org/gnome/shell/extensions/  < $APP_ROOT/dconf/exts.dconf
+if [[ $1 == "load" ]]; then
+  _load
+elif [[ $1 == "dump" ]]; then
+  _dump
+fi
