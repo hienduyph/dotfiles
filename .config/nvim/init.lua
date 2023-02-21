0baf614afef4f1deae59b02bce5421cee13bf609
Hien Pham <hienduyph@gmail.com>
2023-02-21 21:38:18 +0700

    fmt lua stuff

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,10 +1,9 @@
 require("q.01init")
 
 if vim.g.vscode then
-
 else
-  require("q.02plugin")
-  require("q.04cmd")
-  require("q.05keymap")
-  require("q.completion")
+	require("q.02plugin")
+	require("q.04cmd")
+	require("q.05keymap")
+	require("q.completion")
 end
--- .config/nvim/lua/q/04cmd.lua
+++ .config/nvim/lua/q/04cmd.lua
@@ -1,33 +1,36 @@
-vim.api.nvim_create_user_command('GD', 'DiffviewOpen', { nargs = 0 })
-vim.api.nvim_create_user_command('GDC', 'DiffviewClose', { nargs = 0 })
+vim.api.nvim_create_user_command("GD", "DiffviewOpen", { nargs = 0 })
+vim.api.nvim_create_user_command("GDC", "DiffviewClose", { nargs = 0 })
 
-vim.api.nvim_create_user_command('Format', function() vim.lsp.buf.format { async = true } end, { nargs = 0 })
+vim.api.nvim_create_user_command("Format", function()
+	vim.lsp.buf.format({ async = true })
+end, { nargs = 0 })
 
 -- lsp import
 -- refer https://github.com/golang/tools/blob/master/gopls/doc/vim.md#neovim-imports
 local function lsp_organize_imports(timeout_ms)
-  local params = vim.lsp.util.make_range_params()
-  params.context = {only = {"source.organizeImports"}}
-  local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeout_ms)
-  for _, res in ipairs(result or {}) do
-    for _, r in ipairs(res.result or {}) do
-      if r.edit then
-        vim.lsp.util.apply_workspace_edit(r.edit, "UTF-8")
-      else
-        vim.lsp.buf.execute_command(r.command)
-      end
-    end
-  end
+	local params = vim.lsp.util.make_range_params()
+	params.context = { only = { "source.organizeImports" } }
+	local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeout_ms)
+	for _, res in ipairs(result or {}) do
+		for _, r in ipairs(res.result or {}) do
+			if r.edit then
+				vim.lsp.util.apply_workspace_edit(r.edit, "UTF-8")
+			else
+				vim.lsp.buf.execute_command(r.command)
+			end
+		end
+	end
 end
-vim.api.nvim_create_user_command('OR', function() lsp_organize_imports(5000) end, { nargs = 0 })
+vim.api.nvim_create_user_command("OR", function()
+	lsp_organize_imports(5000)
+end, { nargs = 0 })
 
-vim.api.nvim_create_user_command('List', vim.diagnostic.setloclist, { nargs = 0 })
+vim.api.nvim_create_user_command("List", vim.diagnostic.setloclist, { nargs = 0 })
 
 vim.api.nvim_create_user_command("FormatJSON", "%!jq .", { nargs = 0 })
 vim.api.nvim_create_user_command("MinifyJSON", "%!jq -c .", { nargs = 0 })
 
 vim.api.nvim_create_user_command("BufOnly", '%bdelete!|edit #|normal `"', { nargs = 0 })
-vim.api.nvim_create_user_command("Black", '!black %', { nargs = 0 })
+vim.api.nvim_create_user_command("Black", "!black %", { nargs = 0 })
 
-
-vim.cmd [[ autocmd BufWritePre * lua vim.lsp.buf.format { async = true } ]]
+vim.cmd([[ autocmd BufWritePre * lua vim.lsp.buf.format { async = true } ]])
