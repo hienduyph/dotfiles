9853c04499f6ff07b2a583774ecb1cd8c014d070
Hien Pham <hienduyph@gmail.com>
2023-03-09 04:54:40 +0700

    sway config

--- platform/linux/fedora/sway.sh
+++ platform/linux/fedora/sway.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+CUSTOM_EXPORTS=()
+# QT_AUTO_SCREEN_SCALE_FACTOR=0 QT_SCALE_FACTOR=1.5 ELM_SCALE=1.5
+for kv in $(echo $1 | tr " " "\n"); do
+  CUSTOM_EXPORTS+=("export ${kv}")
+done
 
 # https://github.com/fairyglade/ly/issues/228#issuecomment-756742472
 #
@@ -9,10 +14,22 @@ sudo tee /usr/bin/start-sway << EOF
 #!/bin/bash
 
 export SDL_VIDEODRIVER=wayland
+export MOZ_ENABLE_WAYLAND=1
+export MOZ_DISABLE_RDD_SANDBOX=1
+export XDG_SESSION_TYPE=wayland
 export _JAVA_AWT_WM_NONREPARENTING=1
+
+# GTK
+export GDK_BACKEND=wayland
+export CLUTTER_BACKEND=wayland
+
+# qt stuff
 export QT_QPA_PLATFORM=wayland
+export QT_WAYLAND_DECORATION=material
+
 export XDG_CURRENT_DESKTOP=sway
 export XDG_SESSION_DESKTOP=sway
+
 export GTK_IM_MODULE=fcitx
 export QT_IM_MODULE=fcitx
 export XMODIFIERS=@im=fcitx
@@ -20,7 +37,9 @@ export XMODIFIERS=@im=fcitx
 eval \$(/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh)
 export SSH_AUTH_SOCK
 
-exec sway --unsupported-gpu
+${CUSTOM_EXPORTS[@]}
+
+exec sway
 EOF
 
 sudo chmod +x /usr/bin/start-sway
