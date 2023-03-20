b30359d9c09dc9a2438e461af6f8b24ac0d5891d
Hien Pham <hienduyph@gmail.com>
2023-03-20 18:49:30 +0700

    fedora stuff

--- platform/linux/fedora/repo.sh
+++ platform/linux/fedora/repo.sh
@@ -12,7 +12,21 @@ sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
 sudo dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
 
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+sudo tee /etc/yum.repos.d/microsoft.repo << EOF
+[code]
+name=Visual Studio Code
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+
+[edge]
+name=Microsoft Edge
+baseurl=https://packages.microsoft.com/yumrepos/edge
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+EOF
 
 sudo dnf copr enable atim/starship -y
 sudo dnf copr enable atim/lazygit -y
@@ -37,3 +51,4 @@ gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOM
 
 sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+
