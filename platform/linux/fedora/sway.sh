7123eaea317c55793503c5c38e91e7fd6e92c5cf
Hien Pham <hienduyph@gmail.com>
2023-04-20 07:55:48 +0700

    hello rofi wayland again

--- platform/linux/fedora/sway.sh
+++ platform/linux/fedora/sway.sh
@@ -7,7 +7,7 @@ done
 
 # https://github.com/fairyglade/ly/issues/228#issuecomment-756742472
 #
-sudo dnf install -y xdg-desktop-portal xdg-desktop-portal-wlr grim wf-recorder slurp wlr-randr sway dex-autostart waybar fuzzel wev \
+sudo dnf install -y xdg-desktop-portal xdg-desktop-portal-wlr grim wf-recorder slurp wlr-randr sway dex-autostart waybar rofi-wayland wev \
   fcitx5 fcitx5-gtk fcitx5-qt fcitx5-unikey wlsunset pavucontrol blueman mako \
   kvantum swappy
 
