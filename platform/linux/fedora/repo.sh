8178a2b1d8087911e799c0f7534b919e3c0cd54c
Hien Pham <hienduyph@gmail.com>
2023-04-29 09:26:10 +0700

    tweak install & waybar stuff

--- platform/linux/fedora/repo.sh
+++ platform/linux/fedora/repo.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
+_install() {
+  sudo dnf install -y $@
+}
 
 sudo dnf update -y 
 
-sudo dnf install -y curl wget python3 python3-pip python3-devel liberation-fonts dnf-plugins-core https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+_install curl wget python3 python3-pip python3-devel liberation-fonts dnf-plugins-core
+
+_install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+_install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
 sudo rpm --import https://build.opensuse.org/projects/home:manuelschneid3r/public_key
-sudo dnf config-manager --add-repo https://download.opensuse.org/repositories/home:manuelschneid3r/Fedora_36/home:manuelschneid3r.repo
+sudo dnf config-manager --add-repo https://download.opensuse.org/repositories/home:manuelschneid3r/Fedora_37/home:manuelschneid3r.repo
 
 sudo dnf install fedora-workstation-repositories -y
 
