08ed23d8dfd203acb96345cefee3e652b29721d2
Hien Pham <hienduyph@gmail.com>
2023-05-18 07:17:09 +0700

    more conf & remove lapcedb

--- platform/linux/hdpi.sh
+++ platform/linux/hdpi.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer']"
+
 sudo tee /usr/share/glib-2.0/schemas/99_hidpi.gschema.override << EOF
 [org.gnome.desktop.interface]
 scaling-factor=1
