334c06b35b56ad546d2c47a1d637607594e1c11a
Hien Pham <hienduyph@gmail.com>
2023-05-30 23:29:22 +0700

    org pkg stuff

--- platform/linux/fedora/repo.sh
+++ platform/linux/fedora/repo.sh
@@ -6,20 +6,24 @@ _install() {
 sudo dnf update -y 
 
 _install curl wget python3 python3-pip python3-devel liberation-fonts dnf-plugins-core
+sudo dnf remove PackageKit fedora-chromium-config -y
 
 _install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 _install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
+# albert for gnome
 sudo rpm --import https://build.opensuse.org/projects/home:manuelschneid3r/public_key
-sudo dnf config-manager --add-repo https://download.opensuse.org/repositories/home:manuelschneid3r/Fedora_37/home:manuelschneid3r.repo
-
-sudo dnf install fedora-workstation-repositories -y
+sudo dnf config-manager --add-repo https://download.opensuse.org/repositories/home:manuelschneid3r/Fedora_38/home:manuelschneid3r.repo
 
 sudo dnf config-manager --set-enabled google-chrome
 
-sudo dnf remove PackageKit fedora-chromium-config -y
+sudo dnf copr enable atim/joshuto -y
+sudo dnf copr enable atim/starship -y
+sudo dnf copr enable atim/lazygit -y
+sudo dnf copr enable pgaskin/zotero -y
 
-sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
+sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/fedora/hashicorp.repo
 sudo dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
 
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
@@ -38,10 +42,6 @@ enabled=1
 gpgcheck=1
 gpgkey=https://packages.microsoft.com/keys/microsoft.asc
 EOF
-
-sudo dnf copr enable atim/starship -y
-sudo dnf copr enable atim/lazygit -y
-sudo dnf copr enable pgaskin/zotero -y
 
 cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
@@ -62,7 +62,3 @@ repo_gpgcheck=0
 gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOM
 
-sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
-sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/fedora/hashicorp.repo
-
-sudo dnf copr enable atim/joshuto -y
