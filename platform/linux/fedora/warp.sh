85c5e57cf5cc055bfea71ecf771a01ea74304f6b
Hien Pham <hienduyph@gmail.com>
2023-06-17 07:03:03 +0700

    use new pkg manager

--- platform/linux/fedora/warp.sh
+++ platform/linux/fedora/warp.sh
@@ -2,18 +2,7 @@
 
 set -e
 
-sudo rpm -ivh http://pkg.cloudflareclient.com/cloudflare-release-el8.rpm
-
-# fix releasever for fedora
-sudo tee /etc/yum.repos.d/cloudflare.repo << EOM
-[cloudflare]
-name=Cloudflare CentOS Packages
-baseurl=https://pkg.cloudflareclient.com/dists/8/main/binary-\$basearch
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CLOUDFLARE-3
-EOM
+sudo dnf config-manager --add-repo https://pkg.cloudflare.com/cloudflared-ascii.repo
 
 sudo dnf install cloudflare-warp -y
 
