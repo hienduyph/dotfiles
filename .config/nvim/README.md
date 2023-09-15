dea617c0788b7a0b873186468e6e11dc33aaa683
Hien Pham <hienduyph@gmail.com>
2023-09-15 21:14:38 +0700

    nvim: configs more format

--- .config/nvim/README.md
+++ .config/nvim/README.md
@@ -2,44 +2,18 @@
 
 ## Requires
 
-- git: for packer to work
-- `C` & `C++` compiler: for nvim-treesitter compiles grammar.
-- node & npm: needed for some language-server to install.
-- go: to install gopls & work with go project
-
-
+- git: for lazy.nvim to work `brew install git`
+- `C` & `C++` compiler: for nvim-treesitter compiles grammar (`brew install clang`)
+- node & npm: needed for some language-server to install. (`brew install node`)
+- go: to install gopls & work with go project `brew install go`
 
 ## Formatter
+
 - Python: black (`brew install black`)
-- Lua:  https://github.com/JohnnyMorganz/StyLua (`brew install stylua`)
-- Protobuf: https://github.com/bufbuild/buf `brew install bufbuild/buf/buf`
+- Lua: https://github.com/JohnnyMorganz/StyLua (`brew install stylua`)
+- Protobuf: `brew install clang-format`
 - Json: jq (`brew install jq`)
 - SQL: https://github.com/sqlfluff/sqlfluff (`pip install sqlfluff`)
-
-
-```lua
-{
-  "ibhagwan/fzf-lua",
-  config = function()
-    require 'fzf-lua'.setup {
-      fzf_colors = {
-        ["fg"] = { "fg", "Normal" },
-        ["bg"] = { "bg", "Normal" },
-        ["gutter"] = { "bg", "Normal" },
-      },
-      winopts = {
-        preview = {
-          delay = 60,
-        },
-      },
-    }
-    vim.keymap.set("n", "<C-f>", require('fzf-lua').files, { noremap = true, silent = true })
-    vim.keymap.set("n", "<C-g>", require('fzf-lua').live_grep, { noremap = true, silent = true })
-  end
-};
-
-```
-
-
-```lua
-```
+- Rust: `brew install rustfmt`
+- Bash: `brew install shfmt`
+- JS,TS,CSS: `brew install prettier`
--- .config/nvim/lua/q/02plugin.lua
+++ .config/nvim/lua/q/02plugin.lua
@@ -1,387 +1,396 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 local opts = {
-  checker = {
-    -- automatically check for plugin updates
-    enabled = true,
-    notify = false,
-  },
+	checker = {
+		-- automatically check for plugin updates
+		enabled = true,
+		notify = false,
+	},
 }
 
-local fzf = { {
-  'ibhagwan/fzf-lua',
-  config = function()
-    vim.keymap.set("n", "<C-f>", function()
-      require('fzf-lua').files()
-    end, { noremap = true, silent = true })
-    vim.keymap.set("n", "<C-g>", function()
-      require('fzf-lua').live_grep()
-    end, { noremap = true, silent = true })
-    vim.keymap.set("n", "<C-i>", function()
-      require('fzf-lua').buffers()
-    end, { noremap = true, silent = true })
-  end,
-  requires = { 'nvim-tree/nvim-web-devicons' },
-} }
+local fzf = {
+	{
+		"ibhagwan/fzf-lua",
+		config = function()
+			vim.keymap.set("n", "<C-f>", function()
+				require("fzf-lua").files()
+			end, { noremap = true, silent = true })
+			vim.keymap.set("n", "<C-g>", function()
+				require("fzf-lua").live_grep()
+			end, { noremap = true, silent = true })
+			vim.keymap.set("n", "<C-i>", function()
+				require("fzf-lua").buffers()
+			end, { noremap = true, silent = true })
+		end,
+		requires = { "nvim-tree/nvim-web-devicons" },
+	},
+}
 
 local telescope = {
-  { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-  {
-    "nvim-telescope/telescope.nvim",
-    config = function()
-      local tele = require("telescope")
-      tele.setup({
-        defaults = {
-          file_ignore_patterns = { "^.git/" },
-          selection_strategy = "reset",
-          sorting_strategy = "ascending",
-          layout_strategy = "horizontal",
-          layout_config = {
-            horizontal = {
-              prompt_position = "top",
-              preview_width = 0.55,
-              results_width = 0.8,
-            },
-            vertical = {
-              mirror = false,
-            },
-            width = 0.87,
-            height = 0.80,
-            preview_cutoff = 120,
-          },
-        },
-      })
-      tele.load_extension("fzf")
+	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	{
+		"nvim-telescope/telescope.nvim",
+		config = function()
+			local tele = require("telescope")
+			tele.setup({
+				defaults = {
+					file_ignore_patterns = { "^.git/" },
+					selection_strategy = "reset",
+					sorting_strategy = "ascending",
+					layout_strategy = "horizontal",
+					layout_config = {
+						horizontal = {
+							prompt_position = "top",
+							preview_width = 0.55,
+							results_width = 0.8,
+						},
+						vertical = {
+							mirror = false,
+						},
+						width = 0.87,
+						height = 0.80,
+						preview_cutoff = 120,
+					},
+				},
+			})
+			tele.load_extension("fzf")
 
-      vim.keymap.set("n", "<C-f>", function()
-        require("telescope.builtin").find_files({ hidden = true })
-      end, { noremap = true, silent = true })
-      vim.keymap.set("n", "<C-g>", function()
-        require("telescope.builtin").live_grep({ hidden = true })
-      end, { noremap = true, silent = true })
-      vim.keymap.set("n", "<C-i>", function()
-        require("telescope.builtin").buffers()
-      end, { noremap = true, silent = true })
-    end,
-    dependencies = {
-      "nvim-telescope/telescope-fzf-native.nvim",
-    },
-  },
+			vim.keymap.set("n", "<C-f>", function()
+				require("telescope.builtin").find_files({ hidden = true })
+			end, { noremap = true, silent = true })
+			vim.keymap.set("n", "<C-g>", function()
+				require("telescope.builtin").live_grep({ hidden = true })
+			end, { noremap = true, silent = true })
+			vim.keymap.set("n", "<C-i>", function()
+				require("telescope.builtin").buffers()
+			end, { noremap = true, silent = true })
+		end,
+		dependencies = {
+			"nvim-telescope/telescope-fzf-native.nvim",
+		},
+	},
 }
 
 local plugins = {
-  -- core libs
-  "nvim-lua/plenary.nvim",
-  "MunifTanjim/nui.nvim",
-  { "nvim-tree/nvim-web-devicons", config = true },
+	-- core libs
+	"nvim-lua/plenary.nvim",
+	"MunifTanjim/nui.nvim",
+	{ "nvim-tree/nvim-web-devicons", config = true },
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = function()
-      require("nvim-treesitter.install").update({ with_sync = true })
-    end,
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          "vim",
-          "lua",
-          "bash",
-          "ruby",
-          "python",
-          "php",
-          "c",
-          "rust",
-          "go",
-          "gomod",
-          "gosum",
-          "gitignore",
-          "dockerfile",
-          "cpp",
-          "dart",
-          "yaml",
-          "json",
-          "jsonc",
-          "json5",
-          "terraform",
-          "java",
-          "kotlin",
-          "typescript",
-          "javascript",
-          "html",
-          "toml",
-          "markdown",
-          "markdown_inline",
-          "css",
-          "scss",
-          "sql",
-          "proto",
-          "make",
-          "cmake",
-          "latex",
-          "kdl",
-        },
-        autopairs = {
-          enable = true,
-        },
-        indent = {
-          enable = true,
-          disable = { "yaml", "python" },
-        },
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
-      })
-    end,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = function()
+			require("nvim-treesitter.install").update({ with_sync = true })
+		end,
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"vim",
+					"lua",
+					"bash",
+					"ruby",
+					"python",
+					"php",
+					"c",
+					"rust",
+					"go",
+					"gomod",
+					"gosum",
+					"gitignore",
+					"dockerfile",
+					"cpp",
+					"dart",
+					"yaml",
+					"json",
+					"jsonc",
+					"json5",
+					"terraform",
+					"java",
+					"kotlin",
+					"typescript",
+					"javascript",
+					"html",
+					"toml",
+					"markdown",
+					"markdown_inline",
+					"css",
+					"scss",
+					"sql",
+					"proto",
+					"make",
+					"cmake",
+					"latex",
+					"kdl",
+				},
+				autopairs = {
+					enable = true,
+				},
+				indent = {
+					enable = true,
+					disable = { "yaml", "python" },
+				},
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = false,
+				},
+			})
+		end,
+	},
 
-  {
-    "echasnovski/mini.nvim",
-    branch = "main",
-    config = function()
-      require("mini.base16").setup({
-        palette = {
-          base00 = "#ffffff",
-          base01 = "#f6f8eb",
-          base02 = "#dbe1ac",
-          base03 = "#73765a",
-          base04 = "#324490",
-          base05 = "#002a83",
-          base06 = "#0000e4",
-          base07 = "#080500",
-          base08 = "#5e2200",
-          base09 = "#a86400",
-          base0A = "#008818",
-          base0B = "#004500",
-          base0C = "#b34aad",
-          base0D = "#004b76",
-          base0E = "#7d0077",
-          base0F = "#0086ae",
-        },
-        use_cterm = {
-          base00 = 254,
-          base01 = 250,
-          base02 = 246,
-          base03 = 243,
-          base04 = 60,
-          base05 = 18,
-          base06 = 4,
-          base07 = 232,
-          base08 = 52,
-          base09 = 130,
-          base0A = 28,
-          base0B = 22,
-          base0C = 133,
-          base0D = 24,
-          base0E = 90,
-          base0F = 31,
-        },
-      })
-      require("mini.align").setup()
-    end,
-  },
-  { "numToStr/Comment.nvim",       config = true },
-  { "lewis6991/gitsigns.nvim",     config = true },
-  {
-    "nvim-tree/nvim-tree.lua",
-    config = function()
-      require("nvim-tree").setup({
-        git = { enable = false },
-        view = {
-          width = 40,
-        },
-        update_focused_file = {
-          enable = true,
-        },
-        filters = {
-          dotfiles = false,
-          custom = {
-            "^\\.git$",
-            "node_modules",
-            "^\\.cache",
-            "__pycache__",
-            "\\.direnv",
-            "\\.ipynb_checkpoints",
-            "vendor",
-          },
-        },
-        actions = {
-          open_file = {
-            resize_window = false,
-            window_picker = {
-              enable = false,
-            },
-          },
-        },
-      })
-    end,
-  },
-  {
-    "nvim-lualine/lualine.nvim",
-    config = function()
-      require("lualine").setup({
-        options = {
-          theme = "gruvbox",
-          always_show_bufferline = true,
-        },
-        extensions = { "quickfix", "nvim-tree", "fzf" },
-        sections = {
-          lualine_a = { "mode" },
-          lualine_b = { "branch" },
-          lualine_c = {
-            {
-              "filename",
-              file_status = true,
-              path = 1,
-            },
-          },
-          lualine_x = { "encoding", "filetype" },
-          lualine_y = { "progress", "diff" },
-          lualine_z = { "location" },
-        },
-        inactive_sections = {
-          lualine_a = {},
-          lualine_b = {},
-          lualine_c = { "filename" },
-          lualine_x = { "location" },
-          lualine_y = {},
-          lualine_z = {},
-        },
-        tabline = {},
-      })
-    end,
-  },
-  {
-    "akinsho/bufferline.nvim",
-    config = function()
-      require("bufferline").setup({
-        options = {
-          numbers = "ordinal",
-          diagnostics = "nvim_lsp",
-          show_buffer_close_icons = false,
-          show_close_icon = false,
-        },
-      })
-    end,
-  },
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    config = function()
-      require("indent_blankline").setup({
-        char = "|",
-        buftype_exclude = { "terminal", "dashboard" },
-        space_char_blankline = " ",
-        use_treesitter = true,
-        char_list = { "|", "¦", "┆", "┊" },
-        show_current_context = true,
-        show_current_context_start = true,
-      })
-      vim.cmd([[highlight IndentBlanklineChar guifg=#dedede gui=nocombine]])
-      vim.cmd([[highlight IndentBlanklineContextChar guifg=#cfcccc gui=nocombine]])
-    end,
-  },
+	{
+		"echasnovski/mini.nvim",
+		branch = "main",
+		config = function()
+			require("mini.base16").setup({
+				palette = {
+					base00 = "#ffffff",
+					base01 = "#f6f8eb",
+					base02 = "#dbe1ac",
+					base03 = "#73765a",
+					base04 = "#324490",
+					base05 = "#002a83",
+					base06 = "#0000e4",
+					base07 = "#080500",
+					base08 = "#5e2200",
+					base09 = "#a86400",
+					base0A = "#008818",
+					base0B = "#004500",
+					base0C = "#b34aad",
+					base0D = "#004b76",
+					base0E = "#7d0077",
+					base0F = "#0086ae",
+				},
+				use_cterm = {
+					base00 = 254,
+					base01 = 250,
+					base02 = 246,
+					base03 = 243,
+					base04 = 60,
+					base05 = 18,
+					base06 = 4,
+					base07 = 232,
+					base08 = 52,
+					base09 = 130,
+					base0A = 28,
+					base0B = 22,
+					base0C = 133,
+					base0D = 24,
+					base0E = 90,
+					base0F = 31,
+				},
+			})
+			require("mini.align").setup()
+		end,
+	},
+	{ "numToStr/Comment.nvim", config = true },
+	{ "lewis6991/gitsigns.nvim", config = true },
+	{
+		"nvim-tree/nvim-tree.lua",
+		config = function()
+			require("nvim-tree").setup({
+				git = { enable = false },
+				view = {
+					width = 40,
+				},
+				update_focused_file = {
+					enable = true,
+				},
+				filters = {
+					dotfiles = false,
+					custom = {
+						"^\\.git$",
+						"node_modules",
+						"^\\.cache",
+						"__pycache__",
+						"\\.direnv",
+						"\\.ipynb_checkpoints",
+						"vendor",
+					},
+				},
+				actions = {
+					open_file = {
+						resize_window = false,
+						window_picker = {
+							enable = false,
+						},
+					},
+				},
+			})
+		end,
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		config = function()
+			require("lualine").setup({
+				options = {
+					theme = "gruvbox",
+					always_show_bufferline = true,
+				},
+				extensions = { "quickfix", "nvim-tree", "fzf" },
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = { "branch" },
+					lualine_c = {
+						{
+							"filename",
+							file_status = true,
+							path = 1,
+						},
+					},
+					lualine_x = { "encoding", "filetype" },
+					lualine_y = { "progress", "diff" },
+					lualine_z = { "location" },
+				},
+				inactive_sections = {
+					lualine_a = {},
+					lualine_b = {},
+					lualine_c = { "filename" },
+					lualine_x = { "location" },
+					lualine_y = {},
+					lualine_z = {},
+				},
+				tabline = {},
+			})
+		end,
+	},
+	{
+		"akinsho/bufferline.nvim",
+		config = function()
+			require("bufferline").setup({
+				options = {
+					numbers = "ordinal",
+					diagnostics = "nvim_lsp",
+					show_buffer_close_icons = false,
+					show_close_icon = false,
+				},
+			})
+		end,
+	},
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		config = function()
+			require("indent_blankline").setup({
+				char = "|",
+				buftype_exclude = { "terminal", "dashboard" },
+				space_char_blankline = " ",
+				use_treesitter = true,
+				char_list = { "|", "¦", "┆", "┊" },
+				show_current_context = true,
+				show_current_context_start = true,
+			})
+			vim.cmd([[highlight IndentBlanklineChar guifg=#dedede gui=nocombine]])
+			vim.cmd([[highlight IndentBlanklineContextChar guifg=#cfcccc gui=nocombine]])
+		end,
+	},
 
-  {
-    "windwp/nvim-autopairs",
-    config = function()
-      require("nvim-autopairs").setup({
-        check_ts = true,
-        ts_config = {
-          lua = { "string" }, -- it will not add a pair on that treesitter node
-          javascript = { "template_string" },
-          java = false,       -- don"t check treesitter on java
-        },
-      })
-    end,
-  },
+	{
+		"windwp/nvim-autopairs",
+		config = function()
+			require("nvim-autopairs").setup({
+				check_ts = true,
+				ts_config = {
+					lua = { "string" }, -- it will not add a pair on that treesitter node
+					javascript = { "template_string" },
+					java = false, -- don"t check treesitter on java
+				},
+			})
+		end,
+	},
 
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-nvim-lua",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-path",
+			"saadparwaiz1/cmp_luasnip",
 
-  {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    dependencies = {
-      "neovim/nvim-lspconfig",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-nvim-lua",
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-path",
-      "saadparwaiz1/cmp_luasnip",
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
 
-      "WhoIsSethDaniel/mason-tool-installer.nvim",
-      "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
+			"SmiteshP/nvim-navic",
+		},
+	},
+	{
+		"mfussenegger/nvim-dap",
+	},
 
-      "SmiteshP/nvim-navic",
-    },
-  },
-  {
-    'mfussenegger/nvim-dap',
-  },
+	-- specific language
+	{
+		"scalameta/nvim-metals",
+	},
 
-  -- specific language
-  {
-    "scalameta/nvim-metals",
-  },
+	{ "b0o/schemastore.nvim" },
 
-  { "b0o/schemastore.nvim" },
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			"rafamadriz/friendly-snippets",
+		},
+	},
 
-  {
-    "L3MON4D3/LuaSnip",
-    dependencies = {
-      "rafamadriz/friendly-snippets",
-    }
-  },
+	{ "simrat39/rust-tools.nvim", config = true },
+	{ "kylechui/nvim-surround", config = true },
+	"gpanders/editorconfig.nvim",
+	{
+		"nvimdev/guard.nvim",
+		config = function()
+			local ft = require("guard.filetype")
+			ft("python"):fmt("black")
+			ft("lua"):fmt("stylua")
+			ft("json"):fmt({
+				cmd = "jq",
+				stdin = true,
+			})
+			ft("proto,c,cpp"):fmt("clang-format")
+			ft("typescript,javascript,typescriptreact,markdown,html,css,yaml"):fmt("prettier")
+			ft("rust"):fmt({
+				cmd = "rustfmt",
+				args = { "--edition", "2021", "--emit", "stdout" },
+				stdin = true,
+			})
+			ft("sh"):fmt({
+				cmd = "shfmt",
+				stdin = true,
+			})
+			require("guard").setup({
+				fmt_on_save = false, -- only enable for some filetype
+				lsp_as_default_formatter = true,
+			})
+		end,
+	},
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		opts = {},
+	},
+	{
+		"folke/trouble.nvim",
+		requires = "nvim-tree/nvim-web-devicons",
+		config = function()
+			require("trouble").setup({})
+		end,
+	},
 
-  { "simrat39/rust-tools.nvim", config = true },
-  { "kylechui/nvim-surround",   config = true },
-  "gpanders/editorconfig.nvim",
-  {
-    "nvimdev/guard.nvim",
-    config = function()
-      local ft = require('guard.filetype')
-      ft('python'):fmt('black')
-      ft('lua'):fmt('stylua')
-      ft('json'):fmt({
-        cmd = 'jq',
-        stdin = true
-      })
-      ft('typescript,javascript,typescriptreact,markdown'):fmt('prettier')
-
-      require('guard').setup({
-        fmt_on_save = false, -- only enable for some filetype
-        lsp_as_default_formatter = true,
-      })
-    end
-  },
-  {
-    "akinsho/toggleterm.nvim",
-    version = "*",
-    opts = {},
-  },
-  {
-    "folke/trouble.nvim",
-    requires = "nvim-tree/nvim-web-devicons",
-    config = function()
-      require("trouble").setup {
-      }
-    end
-  },
-
-  -- support multi cursor
-  {
-    "mg979/vim-visual-multi",
-  },
+	-- support multi cursor
+	{
+		"mg979/vim-visual-multi",
+	},
 }
 
 table.insert(plugins, telescope)
