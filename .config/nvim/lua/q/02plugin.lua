8f5e8331174f998f1299aecce535af9b37130dc2
Hien Pham <hienpd9@fpt.com>
2023-10-05 18:11:03 +0700

    add hurl

--- .config/nvim/lua/q/02plugin.lua
+++ .config/nvim/lua/q/02plugin.lua
@@ -54,6 +54,7 @@ local plugins = {
 					"yaml",
 					"json",
 					"jsonc",
+					"http",
 					"json5",
 					"terraform",
 					"java",
@@ -311,6 +312,56 @@ local plugins = {
 		requires = "nvim-tree/nvim-web-devicons",
 		config = function()
 			require("trouble").setup({})
+		end,
+	},
+	{
+		"rest-nvim/rest.nvim",
+		requires = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("rest-nvim").setup({
+				-- Open request results in a horizontal split
+				result_split_horizontal = false,
+				-- Keep the http file buffer above|left when split horizontal|vertical
+				result_split_in_place = "vertical",
+				-- Skip SSL verification, useful for unknown certificates
+				skip_ssl_verification = false,
+				-- Encode URL before making request
+				encode_url = true,
+				-- Highlight request on run
+				highlight = {
+					enabled = true,
+					timeout = 150,
+				},
+				result = {
+					-- toggle showing URL, HTTP info, headers at top the of result window
+					show_url = true,
+					-- show the generated curl command in case you want to launch
+					-- the same request via the terminal (can be verbose)
+					show_curl_command = true,
+					show_http_info = true,
+					show_headers = true,
+					-- executables or functions for formatting response body [optional]
+					-- set them to false if you want to disable them
+					formatters = {
+						json = "jq",
+						html = function(body)
+							return vim.fn.system({ "tidy", "-i", "-q", "-" }, body)
+						end,
+					},
+				},
+				-- Jump to request line on run
+				jump_to_request = false,
+				custom_dynamic_variables = {},
+				yank_dry_run = true,
+			})
+
+			vim.keymap.set("n", "<leader>c", function()
+				require("rest-nvim").run()
+			end, { noremap = true, silent = true })
+
+			vim.api.nvim_create_user_command("Curl", function()
+				require("rest-nvim").run(true)
+			end, { nargs = 0 })
 		end,
 	},
 }
