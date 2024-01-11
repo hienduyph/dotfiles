6914e795501a37bac8a459ae789e8c688b59aa53
HienPH <hienduyph@gmail.com>
2024-01-11 08:22:03 +0700

    more plugins go

--- .config/nvim/lua/q/02plugin.lua
+++ .config/nvim/lua/q/02plugin.lua
@@ -267,6 +267,21 @@ local plugins = {
 		"mfussenegger/nvim-jdtls",
 	},
 
+	{
+		"ray-x/go.nvim",
+		dependencies = { -- optional packages
+			"ray-x/guihua.lua",
+			"neovim/nvim-lspconfig",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			require("go").setup()
+		end,
+		event = { "CmdlineEnter" },
+		ft = { "go", "gomod" },
+		-- build = ':lua require("go.install").update_all_sync()',
+	},
+
 	{ "b0o/schemastore.nvim" },
 
 	{
