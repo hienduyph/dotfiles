b02e3fbab7b8c4bea6168bf2b139e2d4642e612f
HienPH <hienduyph@gmail.com>
2024-02-15 11:38:32 +0700

    add btop

--- .config/nvim/lua/q/ide.lua
+++ .config/nvim/lua/q/ide.lua
@@ -47,8 +47,9 @@ dap.configurations.scala = {
 local metals_config = require("metals").bare_config()
 metals_config.settings = {
 	showImplicitArguments = true,
-	useGlobalExecutable = true,
+	useGlobalExecutable = false,
 	excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
+	serverVersion = "latest.snapshot",
 }
 metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
 metals_config.on_attach = function(client, bufnr)
