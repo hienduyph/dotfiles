2dd72a002ebf244d428d32a89d6079a5c8c72775
HienPH <hienduyph@gmail.com>
2024-03-23 12:30:08 +0700

    nvim: update plugins

--- .config/nvim/lua/q/02plugin.lua
+++ .config/nvim/lua/q/02plugin.lua
@@ -23,6 +23,7 @@ local plugins = {
 	-- core libs
 	"nvim-lua/plenary.nvim",
 	"MunifTanjim/nui.nvim",
+	"nvim-neotest/nvim-nio",
 	{ "nvim-tree/nvim-web-devicons", config = true },
 
 	{
@@ -361,56 +362,6 @@ local plugins = {
 					vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "Visual" })
 				end,
 			})
-		end,
-	},
-	{
-		"rest-nvim/rest.nvim",
-		requires = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("rest-nvim").setup({
-				-- Open request results in a horizontal split
-				result_split_horizontal = false,
-				-- Keep the http file buffer above|left when split horizontal|vertical
-				result_split_in_place = "vertical",
-				-- Skip SSL verification, useful for unknown certificates
-				skip_ssl_verification = false,
-				-- Encode URL before making request
-				encode_url = true,
-				-- Highlight request on run
-				highlight = {
-					enabled = true,
-					timeout = 150,
-				},
-				result = {
-					-- toggle showing URL, HTTP info, headers at top the of result window
-					show_url = true,
-					-- show the generated curl command in case you want to launch
-					-- the same request via the terminal (can be verbose)
-					show_curl_command = true,
-					show_http_info = true,
-					show_headers = true,
-					-- executables or functions for formatting response body [optional]
-					-- set them to false if you want to disable them
-					formatters = {
-						json = "jq",
-						html = function(body)
-							return vim.fn.system({ "tidy", "-i", "-q", "-" }, body)
-						end,
-					},
-				},
-				-- Jump to request line on run
-				jump_to_request = false,
-				custom_dynamic_variables = {},
-				yank_dry_run = true,
-			})
-
-			vim.keymap.set("n", "<leader>c", function()
-				require("rest-nvim").run()
-			end, { noremap = true, silent = true })
-
-			vim.api.nvim_create_user_command("Curl", function()
-				require("rest-nvim").run(true)
-			end, { nargs = 0 })
 		end,
 	},
 }
