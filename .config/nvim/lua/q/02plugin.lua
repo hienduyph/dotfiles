e7f88477ee6983e065587ac5e64dcbf286e0d5f4
Hien Pham <hienduyph@gmail.com>
2024-04-15 05:01:23 +0700

    move right nvim tree

--- .config/nvim/lua/q/02plugin.lua
+++ .config/nvim/lua/q/02plugin.lua
@@ -107,6 +107,7 @@ local plugins = {
 				git = { enable = false },
 				view = {
 					width = 40,
+          side = "right",
 				},
 				update_focused_file = {
 					enable = true,
@@ -325,10 +326,42 @@ local plugins = {
 	},
 	{
 		"folke/trouble.nvim",
-		requires = "nvim-tree/nvim-web-devicons",
 		config = function()
 			require("trouble").setup({})
 		end,
+		branch = "dev", -- IMPORTANT!
+		keys = {
+			{
+				"<leader>xx",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"<leader>xX",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<leader>cs",
+				"<cmd>Trouble symbols toggle focus=false<cr>",
+				desc = "Symbols (Trouble)",
+			},
+			{
+				"<leader>cl",
+				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+				desc = "LSP Definitions / references / ... (Trouble)",
+			},
+			{
+				"<leader>xL",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "Location List (Trouble)",
+			},
+			{
+				"<leader>xQ",
+				"<cmd>Trouble qflist toggle<cr>",
+				desc = "Quickfix List (Trouble)",
+			},
+		},
 	},
 	{
 		"RRethy/vim-illuminate",
