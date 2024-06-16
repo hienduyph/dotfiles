90fe12b8d16639ff9305a858421fbd98c084ad4c
Hien Pham <hienduyph@gmail.com>
2024-06-16 14:48:23 +0700

    update core pkgs

--- platform/linux/fedora/repo.sh
+++ platform/linux/fedora/repo.sh
@@ -5,28 +5,16 @@ _install() {
 
 sudo dnf update -y 
 
-_install curl wget python3 python3-pip python3-devel  dnf-plugins-core
+_install curl wget python3 python3-pip python3-devel dnf-plugins-core
 sudo dnf remove PackageKit fedora-chromium-config -y
 
 _install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 _install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-sudo dnf copr enable atim/joshuto -y
 sudo dnf copr enable atim/starship -y
-sudo dnf copr enable atim/lazygit -y
 
 sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
-
-# sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/fedora/hashicorp.repo
-
-cat <<EOF | sudo tee /etc/yum.repos.d/hashicorp.repo
-[hashicorp]
-name=Hashicorp Stable - \$basearch
-baseurl=https://rpm.releases.hashicorp.com/fedora/39/\$basearch/stable
-enabled=1
-gpgcheck=1
-gpgkey=https://rpm.releases.hashicorp.com/gpg
-EOF
+sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/fedora/hashicorp.repo
 
 
 cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
