f3ec9832f9b8543d11e5d0207249ecae75b1325f
Hien Pham <hienduyph@gmail.com>
2024-07-07 08:22:12 +0700

    update shell

--- shell/alias.sh
+++ shell/alias.sh
@@ -21,6 +21,7 @@ alias se="sudoedit"
 alias yt-mp3="yt-dlp --extract-audio --audio-format mp3"
 alias yt-lossless="yt-dlp --extract-audio --audio-format best"
 
+alias tm="tmux new-session -A -s $(hostname -s)"
 alias q="tmux new-session -A -s Lala"
 
 fetch_git_ignore() {
